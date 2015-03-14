6ff87b13c3daa91d74195bbfdcf7e222d6a34d1c
Lloyd H. Meinholz <meinholz@javabilities.com>
2015-03-13 23:11:22 -0400

    Added different bootstrap style, added about and home pages.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,19 @@
 console.log('\'Allo \'Allo!');
+
+$('#home').click(function (e) {
+  console.log('home was clicked.');
+  e.preventDefault();
+  $('#page-content').load('_home.html');
+});
+
+$('#time-tracker').click(function (e) {
+  console.log('time-tracker was clicked.');
+  e.preventDefault();
+  $('#page-content').load('_time_tracker.html');
+});
+
+$('#about').click(function (e) {
+  console.log('about was clicked.');
+  e.preventDefault();
+  $('#page-content').load('_about.html');
+});
